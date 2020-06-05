9adde8adebee89f551209607bb90d884f5622806
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-05 14:31:06 +0600

    registration is completed with password hashing

--- source/auth/routes/auth.js
+++ source/auth/routes/auth.js
@@ -1,18 +1,24 @@
 const router = require("express").Router();
 const User = require("../model/User");
-
+const hash = require('object-hash');
 
 router.post("/register", async (req, res) => {
+
+    const hashedPassword = hash(req.body.password);
 
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({
+            name: savedUser.name,
+            email: savedUser.email,
+            success: true
+        });
     } catch(err) {
         res.status(400).send(err);
     }
