af84af468baed2797762a9c619b1f6b1e696fffa
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-05 16:31:08 +0600

    login functionality is completed

--- source/auth/routes/auth.js
+++ source/auth/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const hash = require('object-hash');
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
 
@@ -22,6 +23,26 @@ router.post("/register", async (req, res) => {
     } catch(err) {
         res.status(400).send(err);
     }
+});
+
+
+router.post("/login", async (req, res) => {
+
+    const hashedPassword = hash(req.body.password);
+
+    const emailExist = await User.findOne({email: req.body.email});
+    if(!emailExist) res.status(400).send("invalid authentication credential");
+
+    console.log(hashedPassword);
+    console.log(emailExist.password);
+    if(hashedPassword === emailExist.password) {
+        console.log(emailExist.id);
+        const token = jwt.sign({id: emailExist.id}, process.env.TOKEN_SECRET);
+        res.header('auth-token', token).send(token);
+    } else {
+        res.status(400).send("invalid authentication credential");
+    }
+
 });
 
 
